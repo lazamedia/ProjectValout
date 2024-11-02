66400273ade5cce89fae45ac11f93ab2436f2fed
lazuardi <lazamediamxt@gmail.com>
2024-11-02 19:58:21 +0700

    police

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -8,15 +8,20 @@ use App\Models\User;
 class ProjectPolicy
 {
     /**
-     * Determine if the given project can be edited or deleted by the user.
+     * Determine if the given project can be edited by the user.
      */
     public function update(User $user, Project $project)
     {
-        return $user->id === $project->user_id;
+        // Izinkan akses jika pengguna adalah admin atau pemilik proyek
+        return $user->role === 'admin' || $user->id === $project->user_id;
     }
 
+    /**
+     * Determine if the given project can be deleted by the user.
+     */
     public function delete(User $user, Project $project)
     {
-        return $user->id === $project->user_id;
+        // Izinkan akses jika pengguna adalah admin atau pemilik proyek
+        return $user->role === 'admin' || $user->id === $project->user_id;
     }
 }
